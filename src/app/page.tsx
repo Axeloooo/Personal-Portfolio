4f6f0a8dce46e93732480d0779513333a7f162f5
Axeloooo <axelshz@gmail.com>
2023-11-04 00:26:32 -0600

    feature/intro completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import Intro from "@/components/Intro";
+import SectionDivider from "@/components/SectionDivider";
+import React from "react";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center"></main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro></Intro>
+      <SectionDivider></SectionDivider>
+    </main>
+  );
 }
---
+++ src/components/Intro.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
+
+function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/axel.png"
+              alt="Axel portrait"
+              width="192"
+              height="192"
+              quality={95}
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-white shadow-xl border-[0.35rem]"
+            ></Image>
+          </motion.div>
+
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋🏻
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.p
+        className="font-medium !leading-[1,5] text-2xl sm.text-4xl mt-4 mb-10 px-4"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        Hi! I'm <span className="font-bold">Axel</span>, a{" "}
+        <span className="font-bold">full-stack developer</span> based in{" "}
+        <span className="font-bold">Paris, France</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"></BsArrowRight>
+        </Link>
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full text-gray-700 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://www.linkedin.com/in/axel-s%C3%A1nchez-a1089b23a/"
+          target="_blank"
+        >
+          <BsLinkedin></BsLinkedin>
+        </a>
+        <a
+          className="bg-white p-4 flex items-center gap-2 rounded-full text-gray-700 text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+          href="https://github.com/Axeloooo"
+          target="_blank"
+        >
+          <FaGithubSquare></FaGithubSquare>
+        </a>
+      </motion.div>
+    </section>
+  );
+}
+
+export default Intro;
